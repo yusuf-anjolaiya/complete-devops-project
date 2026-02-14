c154bd74c2ea83835777113c6a6403409e751651
Oluwatobi Anjolaiya <oluwatobianjolaiya@Oluwatobis-MacBook-Pro.local>
2026-02-13 20:57:21 -0600

    adding more configs.

--- terraform-configs/provider.tf
+++ terraform-configs/provider.tf
@@ -1,5 +1,12 @@
 provider "kubernetes" {
-    host = minikube_cluster.minikube_docker.client_certificate
+     
+    host = minikube_cluster.minikube_docker.host
+    client_certificate = minikube_cluster.minikube_docker.client_certificate
     client_key = minikube_cluster.minikube_docker.client_key
     cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
+}
+provider "helm" {
+  kubernetes={
+    config_path = "~/.kube/config"
+  }
 }
